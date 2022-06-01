fd07c0eb2a8c1876aff83324dcff22dd59eca832
burdey <88636823+burdey@users.noreply.github.com>
2022-06-01 11:50:14 +0800

    Update zoo.cpp

--- zoo.cpp
+++ zoo.cpp
@@ -6,16 +6,16 @@
 
 zoo::zoo(string n,int cows,int lions){
     name = n;
-    int numbers_of_animals = cows+lions;
+    int number_of_animals = cows+lions;
 
-    animals = new animal* [numbers_of_animals];
-    for(int i=0; i<numbers_of_animals; i++){
+    animals = new animal* [number_of_animals];
+    for(int i=0; i<number_of_animals; i++){
         if(i<cows){
             vegie* ani = new vegie("Daisy",100);
             animals[i] = ani;
         }
         else{
-            hunter* ani - new hunter("Clarence",50);
+            hunter* ani = new hunter("Clarence",50);
             animals[i] = ani;
         }
     }
@@ -25,10 +25,10 @@ string zoo::get_name(){
     return name;
 }
 
-int zoo::get_numbers_of_animals(){
-    return numbers_of_animals;
+int zoo::get_number_of_animals(){
+    return number_of_animals;
 }
 
 animal** zoo::get_animal(){
-    return animal;
+    return animals;
 }
